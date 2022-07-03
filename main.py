91e1c600a55606557bf3bae12cc02f7c57c19fe3
ycoloma-etionic <ycoloma@etionic.com>
2022-07-03 17:42:04 -0300

    added basic keypress detection

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
 #!/usr/bin/env python3
 import tcod
 
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
+    player_x = int(screen_width/2)
+    player_y = int(screen_height/2)
+
     tileset = tcod.tileset.load_tilesheet(
         "assets/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+
+    event_handler = EventHandler()
 
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="PythonGame 1",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="RE")
 
             context.present(root_console)
 
+            root_console.clear()
+
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
                     raise SystemExit()
 
 
